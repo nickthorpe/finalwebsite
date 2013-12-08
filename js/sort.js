99c510d6b98b49db6a680b18d451cb09070d31db
CalebKim <dthturretbuddy@gmail.com>
2013-12-08 04:54:57 -0800

    no message

--- js/sort.js
+++ js/sort.js
@@ -2,22 +2,22 @@
 // render the list of events
 
 $(function(){
-    render(Events.Concert, '.events_concert');
-    render(Events.Education, '.events_education');
-    render(Events.Sports, '.events_sports');
-    render(Events.Community, '.events_community');
-
+    render(Events.Concert, '.events_concert', 'concert');
+    render(Events.Education, '.events_education', 'education');
+    render(Events.Sports, '.events_sports', 'sports');
+    render(Events.Community, '.events_community', 'community');
+    remove();
 });
 
 //function to render the events
-function render(entries, type){
+function render(entries, type, indicator){
 
     var tem= $('.template');
     var events= $(type);
     var instance;
     events.hide();
     events.empty();
-
+    var idx = 0;
     $.each(entries, function(){
         instance = tem.clone();
         instance.find('.name').html(this.name);
@@ -35,12 +35,18 @@ function render(entries, type){
 
         instance.find('.pic').attr({
             src: this.pic,
-            alt: 'Picture of ' + this.name
+            alt: 'Picture of ' + this.name,
+            id: indicator + idx
         });
 
         instance.removeClass('template');
         events.append(instance);
         events.fadeIn("slow");
+        idx = 1 + idx;
     });
 }
 
+function remove () {
+    var x = $('#community5').parent().parent();
+    x.remove("hr").next('hr').remove();
+}
