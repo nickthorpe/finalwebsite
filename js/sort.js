787bedb8b3d7328a0beb42e801de396b6b8f57dc
Phongsakorn <iphong@uw.edu>
2013-12-10 05:02:02 -0800

    search

--- js/sort.js
+++ js/sort.js
@@ -8,15 +8,79 @@ $(function(){
     render(Events.Community, '.events_community', 'community');
     remove();
     var $scrollingDiv = $("#scrollingDiv");
+
+    //search 
+    $('.search').click(function(){
+        var input = $('.input').val();
+        // search for all the events
+        // search(Events.Concert,input);
+        // search(Events.Education,input);
+        // search(Events.Sports,input);
+        // search(Events.Community,input);
+    });
+
+    $('.input').keyup(function() {
+        
+        //empty the search list        
+         $('.search_list').empty(); 
+        var search_term = $(this).val().toLowerCase();
+        // run the search
+        search(Events.Concert,search_term);
+        search(Events.Sports,search_term);
+        search(Events.Education,search_term);
+        search(Events.Community,search_term);
+        
+    })
+
  
-        $(window).scroll(function(){            
-            $scrollingDiv
-                .stop()
-                .animate({"marginTop": ($(window).scrollTop() + 30) + "px"}, "slow" );          
-        });
+    $(window).scroll(function(){            
+        $scrollingDiv
+            .stop()
+            .animate({"marginTop": ($(window).scrollTop() + 30) + "px"}, "slow" );          
+    });
+
+
 });
 
-//function to render the events
+function search(entries, search_term) {
+
+    results = [];
+    id_array = []; //store id
+
+    if (search_term != '') {
+        // search_results_box.html('geelert');
+        $.each(entries, function() {
+            var name = this.name;
+            var id = this.id;
+            var titleLowerCase = name.toLowerCase();
+            
+            if (titleLowerCase.indexOf(search_term) !== -1) {
+                results.push([name]);
+                id_array.push([id]);
+            };
+        });
+        //print the results
+        printResults(results, id_array);
+    } else{
+        //clear the searchbox
+        $('input').html();
+        results = [];
+
+        printResults(results, id_array);
+    };
+
+}
+
+function printResults(results, id_array) {
+
+    for (var i=0;i<results.length;i++)
+    {
+        //print the result and id in the search list
+        $('.search_list').append("<li><a href='feed.html#" +  id_array[i] + "'>" + results[i] + "</a></li>");
+    }
+}
+
+//function to render the events_community
 function render(entries, type, indicator){
 
     var tem= $('.template');
