318243e45a7f1eacf54e6f5243a6b50c6d4aa590
Phongsakorn <iphong@uw.edu>
2013-12-10 00:40:46 -0800

    render home page json

---
+++ js/sort_home_list.js
@@ -0,0 +1,43 @@
+
+// render the list of events
+
+$(function(){
+    render(Events.Concert, '.concert', 'concert');
+    render(Events.Education, '.education', 'educations');
+    render(Events.Sports, '.sport', 'sports');
+});
+
+//function to render the list of events on the home page
+function render(entries, type, indicator){
+
+    var tem= $('.template_feature');
+    var events= $(type);
+    var instance;
+    events.hide();
+    events.empty();
+    var idx = 0;
+    var count = 1;
+    instance = tem.clone();
+
+    $.each(entries, function(){
+
+        if (this.feature == true)
+        { 
+            instance.find('.link' + count).html(this.feature_name);
+            instance.find('.link' + count).attr({
+                href: 'feed.html#'+ indicator + idx
+            });
+
+            instance.find('.link_sub').html(this.date);
+
+            count++;
+            instance.removeClass('template_feature');
+            events.append(instance);
+            events.fadeIn("slow");
+
+        }
+        idx = 1 + idx;
+
+   });
+
+}
